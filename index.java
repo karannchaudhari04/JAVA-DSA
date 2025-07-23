0ca84f441193664f8ba06034c31dcd8cd2ec6ff2
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-07-23 16:16:01 +0530

    initial commit

--- index.java
+++ index.java
@@ -1,3 +1,6 @@
 public class index {
-    
+    public static void main(String[] args) {
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
 }
