93d8accf00b44f18a88c585f16c06cd6c47ea964
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-07-23 16:16:35 +0530

    Write my first program in java

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 public class index {
     public static void main(String[] args) {
         // This is a simple Java program that prints "Hello, World!" to the console.
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); 
     }
 }
