8ba01c31967dfcc2b62184d4f95faa0e1f3039a5
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-07-24 20:12:28 +0530

    initial commit

--- index.java
+++ index.java
@@ -1,20 +1,9 @@
 public class index {
     public static void main(String[] args) {
-        // This is a simple Java program that prints "Hello, World!" to the console.
         System.out.println("Hello, World!"); 
         int a = 20;
         int b = 30;
         int sum = a+b;
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
-        // This is a comment explaining the next line of code.
-        // The following line prints the sum of two numbers.
-        System.out.println("This is a simple Java program demonstrating basic syntax.");
-        // You can modify the values of 'a' and 'b' to see different results
-        // Feel free to add more functionality or modify the code as needed.
-        // This program serves as a basic template for Java applications.
-        // End of the program
-        // You can run this program in any Java IDE or online compiler. 
-        // Make sure to have the Java Development Kit (JDK) installed to compile and run this code.
-        // Happy coding!
     }
 }
