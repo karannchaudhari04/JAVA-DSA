6e087fb6b2a049ae3637938c152740685e3698dc
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-07-24 20:11:08 +0530

    initial commit

--- index.java
+++ index.java
@@ -2,5 +2,9 @@ public class index {
     public static void main(String[] args) {
         // This is a simple Java program that prints "Hello, World!" to the console.
         System.out.println("Hello, World!"); 
+        int a = 20;
+        int b = 30;
+        int sum = a+b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
 }
