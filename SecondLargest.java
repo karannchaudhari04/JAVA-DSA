a44d7bc1a90b15b707c50f87b5577914b6e03b65
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-09-01 20:23:33 +0530

    code completed with best approach

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,31 +1,26 @@
 import java.util.*;
 
 public class SecondLargest{
-    public static void main(String[] args) {
-        System.out.println("Enter the size of the array:");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter the elements of the array:");
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++) {
-            if(arr[i] > largest) {
+    static int getSecondLargest(int[] arr){
+        int n = arr.length;
+
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int i =0; i < n; i++){
+            if(arr[i]> largest){
                 secondLargest = largest;
                 largest = arr[i];
-            } else if(arr[i] > secondLargest && arr[i] != largest) {
-                secondLargest = arr[i];
             }
-            
+            else if(arr[i] < largest && arr[i] > secondLargest){
+                secondLargest = arr[i];
         }
-        if(secondLargest == Integer.MIN_VALUE) {
-            System.out.println("There is no second largest element.");
-        } else {
-            System.out.println("The second largest element is: " + secondLargest);
-        }
-        sc.close();
     }
+
+    return secondLargest;
+}
+public static void main(String[] args) {
+    int[] arr = {12, 35, 1, 10, 34, 1};
+    System.out.println(getSecondLargest(arr));
+}
 }
