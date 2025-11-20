5e60e3cf4295bf5c7df0c5c36e332590b4fbf12d
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-11-20 15:59:48 +0530

    Solved Find the gretest from 2 numbers problem

---
+++ areaofcircle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class areaofcircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of circle: ");
+        int radius = sc.nextInt();
+
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle is: " + area);
+    }
+}
