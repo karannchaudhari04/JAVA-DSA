732af0a3b35828b71003b0f8ac33af7a4c45d39d
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-12-23 20:10:45 +0530

    write simple pattern code

---
+++ Basics/pattern.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class pattern {
+    public static void main(String[] args) {
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
