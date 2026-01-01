659bf43f0a9e14ce83bd8cab20982a5b78eb6d91
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-01 17:02:47 +0530

    datatypes.java

---
+++ Basics/datatypes.java
@@ -0,0 +1,27 @@
+package Basics;
+
+public class datatypes {
+    public static void main(String[] args) {
+        byte b = 1;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        int num = 10;
+        System.out.println(num);
+
+        boolean var = true;
+        System.out.println(var);
+
+        float f = 9;
+        System.out.println(f);
+
+        double d = 10000.4;
+        System.out.println(d);
+
+        long l = 6654;
+        System.out.println(l);
+
+    }
+}
