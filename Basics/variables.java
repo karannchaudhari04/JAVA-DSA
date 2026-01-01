0430568150c5bd0a1b39a89e77bc142990338c21
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-01 16:45:11 +0530

    variables.java

---
+++ Basics/variables.java
@@ -0,0 +1,17 @@
+package Basics;
+import java.util.*;
+public class variables {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        String name = "Karan Chaudhari";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+        a = b;
+        System.out.println(a);
+    }
+}
