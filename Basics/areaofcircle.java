eb34cfba334977122f450f5ff4962f3212d850ec
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-05 20:18:45 +0530

    initial commit

--- Basics/areaofcircle.java
+++ Basics/areaofcircle.java
@@ -1,5 +1,11 @@
 package Basics;
 import java.util.*;
 public class areaofcircle {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle:");
+        double radius = sc.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.println(area);
+    }
 }
