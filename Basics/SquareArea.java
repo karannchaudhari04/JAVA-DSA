dfca6e2511116be49fd1071996f2ff0b859824c1
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-10 12:53:08 +0530

    Area of sqare

---
+++ Basics/SquareArea.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.Scanner;
+
+public class SquareArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the side of the square: ");
+        double side = sc.nextDouble();
+
+        double area = side * side;
+
+        System.out.println("Area of the square = " + area);
+    }
+}
