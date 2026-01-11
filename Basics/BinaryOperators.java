8a6e8ce7086e0a792facff2afece27bd44cede7a
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-11 13:20:24 +0530

    Binary Operators in java

---
+++ Basics/BinaryOperators.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class BinaryOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum = "+(a+b));
+        System.out.println("Subtract = "+(b-a));
+        System.out.println("Multiply = "+(a*b));
+        System.out.println("Division = "+(b/a));
+        System.out.println("Modulo = "+(b%a));
+
+    }
+}
