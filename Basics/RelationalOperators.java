a0b94f31c86eba84570e02f50756a954f0ac96f0
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-11 13:41:55 +0530

    Relational Operators in java

---
+++ Basics/RelationalOperators.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
