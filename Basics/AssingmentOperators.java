d613ff3532aeed0d0c9e660d09224451c29f180a
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-11 13:55:47 +0530

    Assingment Operators in java

---
+++ Basics/AssingmentOperators.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class AssingmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 10;
+        System.out.println(a);
+        int b = 5;
+        b -= 5;
+        System.out.println(b);
+        int c = 10;
+        c *= 5;
+        System.out.println(c);
+        int d = 10;
+        d /= 10;
+        System.out.println(d);
+    }
+}
