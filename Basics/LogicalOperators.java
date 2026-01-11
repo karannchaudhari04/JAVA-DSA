4b0927dcbb67b7826f58a3c2d8a03385fd03a3e1
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-11 13:49:39 +0530

    Logical Operators in java

---
+++ Basics/LogicalOperators.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        System.out.println(3>2 && 2>=2);
+        System.out.println(10>5 || 5>4);
+        System.out.println(!(10==10));
+    }
+}
