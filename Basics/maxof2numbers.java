597dc0ac4e75d7cd1372943de5095e98ef91dde6
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-13 16:55:36 +0530

    maxof 2 numbers

---
+++ Basics/maxof2numbers.java
@@ -0,0 +1,18 @@
+package Basics;
+import java.util.Scanner;
+
+public class maxof2numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if(num1 > num2){
+            System.out.println(num1+" is largest number");
+        }
+        else if(num1 == num2){
+            System.out.println(num1+" & "+num2+" are equal numbers");
+        }
+    }
+}
