20e2a226fe494a7a27b3519d16f544ec5f94baae
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-23 12:34:18 +0530

    intial commit

---
+++ Basics/dowhile.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class dowhile {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println(count);
+            count++;
+        } while (count <= 10);
+    }
+}
