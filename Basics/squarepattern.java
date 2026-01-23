9331dea1ed1decc8ee552361d65f90e3074d58bf
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-23 12:00:03 +0530

    initial commit

---
+++ Basics/squarepattern.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class squarepattern {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i =1; i<=n; i++){
+            System.out.println("****");
+        }
+    }
+}
