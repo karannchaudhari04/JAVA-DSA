450baf8214151e533b12cbf9f23673b9513b8982
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-23 12:51:12 +0530

    initial commit

---
+++ Basics/breakstatement.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.*;
+public class breakstatement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+            if (num % 10 == 0) {
+                break;
+            }
+            System.out.println(num);
+        }while(true);
+    }
+}
