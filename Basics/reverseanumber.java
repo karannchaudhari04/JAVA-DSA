7748fb7642ff55da3edf1699230851790e99ca03
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-23 12:26:02 +0530

    initial commit

--- Basics/reverseanumber.java
+++ Basics/reverseanumber.java
@@ -3,10 +3,12 @@ package Basics;
 public class reverseanumber {
     public static void main(String[] args) {
         int n = 110899;
+        int rev = 0;
         while(n>0){
             int lastdigit = n%10;
-            System.out.print(lastdigit);
+            rev = rev * 10 + lastdigit;
             n = n/10;
         }
+        System.out.println(rev);
     }
 }
