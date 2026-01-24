2ee8794f133fb8d3034f5a5cc8d71c69d92e4434
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-24 11:32:13 +0530

    pattern

--- index.java
+++ index.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
         int n =5;
-        for(int i=n; i>=1; i--){
+        for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
-                System.out.print("*");
+                System.out.print(j);
             }
             System.out.println();
         }
