6c536f03407b96ffcc7daf3ce99df79a2ebe4adc
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-24 10:49:09 +0530

    factorial injava

--- Basics/factorial.java
+++ Basics/factorial.java
@@ -1,8 +1,12 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class factorial {
     public static void main(String[] args) {
-        int number = 4;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to calculate its factorial: ");
+        int number = sc.nextInt();
         int factorial = 1;
         for (int i = 1; i <= number; i++) {
             factorial *= i;
