e5918d3465d32878bb74d6a28b9237dcb79f915d
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-24 10:47:07 +0530

    initial commit

---
+++ Basics/prime.java
@@ -0,0 +1,28 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if(n == 2){
+            System.out.println(n + " is a prime number.");
+        }
+        else{
+            boolean isPrime = true;
+            for (int i = 2; i <= n-1; i++) {
+                if (n % i == 0) {
+                isPrime = false;
+                }
+            }
+            if (isPrime == true) {
+                System.out.println(n + " is a prime number.");
+            } else {
+                System.out.println(n + " is not a prime number.");
+            }
+        }
+    }
+}
