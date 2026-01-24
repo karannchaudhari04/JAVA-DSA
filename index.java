12b3de8b55a0e30c6602e0d6b65aedf51dd7bafa
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-24 10:55:24 +0530

    Hii

--- index.java
+++ index.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
-        for(int i=0; i<5; i++){
-            System.out.println("Hello, World!");
-            i+=2;
+        // Write a program that reads a set of integers, and then prints the sum of the even and odd integers
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of integers you want to input: ");
+        int n = sc.nextInt();
+        int evenSum = 0;
+        int oddSum = 0;
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            int number = sc.nextInt();
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
         }
+        System.out.println("Sum of even integers: " + evenSum);
+        System.out.println("Sum of odd integers: " + oddSum);
     }
 }
