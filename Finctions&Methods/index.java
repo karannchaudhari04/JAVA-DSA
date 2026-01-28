2f58df344be0330d922a72fdb0f3405e9f535d8e
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-28 11:29:09 +0530

    Function syntax

---
+++ Finctions&Methods/index.java
@@ -0,0 +1,10 @@
+public class index {
+    public static void printHello(){
+        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
+        return;
+    }
+    public static void main(String[] args) {
+        printHello(); //function call
+    }
+}
