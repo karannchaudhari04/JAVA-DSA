3e612ece7b2d311d064ea0d85ff248edc0256cce
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-29 20:30:31 +0530

    Product of a and b using method

---
+++ Finctions&Methods/product.java
@@ -0,0 +1,9 @@
+public class product {
+    public static void multiply(int a, int b){
+        int product = a * b;
+        System.out.println("Product is: " + product); 
+    }
+    public static void main(String[] args) {
+        multiply(5, 10); //method call
+    }
+}
