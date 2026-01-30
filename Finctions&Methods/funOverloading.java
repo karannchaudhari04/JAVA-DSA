69f1b9adbe2bc9cb4ca4fcf9d7af03e888b5e44b
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-30 20:30:00 +0530

    function overloading using different datatypes

--- Finctions&Methods/funOverloading.java
+++ Finctions&Methods/funOverloading.java
@@ -6,8 +6,20 @@ public class funOverloading{
     public static int sum(int a, int b, int c){
         return a+b+c;
     }
+
+    // Function Overloading Using Different Data Types
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static double add(double a, double b){
+        return a+b;
+    }
+
+
     public static void main(String argsp[]){
         System.out.println(sum(10, 10));
-        System.out.println(sum(10, 10, 10));
+        System.out.println(sum(10, 5, 5));
+        System.out.println(add(10, 20));
+        System.out.println(add(9.5, 20.5));
     }
 }
