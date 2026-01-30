df20c41a802fa7979fbd82ac9b483f735407bea1
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-01-30 23:53:31 +0530

    Prime or not using function

---
+++ Finctions&Methods/prime.java
@@ -0,0 +1,16 @@
+public class prime {
+    public static boolean isPrime(int n){
+        if(n <= 1){
+            return false;
+        }
+        for(int i = 2; i<n-1; i++){
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(5));
+    }
+}
