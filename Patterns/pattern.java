00f08984f2a445b4c11f1788c683baeacdab3814
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-02 20:28:28 +0530

    inverted rotated half pyramid

--- Patterns/pattern.java
+++ Patterns/pattern.java
@@ -1,12 +1,32 @@
 package Patterns;
 
 public class pattern {
-    public static void main(String[] args) {
-       for(int i=1; i<=5; i++){
+
+    public static void halfPyramid(int n){
+        for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
             System.out.print("*");
         }
         System.out.println();
        }
+    }
+    public static void inverted_Half_Pyramid(int n){
+        //outer loop - rows
+        for(int i=1; i<=n; i++){
+        //inner loop - columns
+            //for spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //for stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+        System.out.println();
+       }
+    }
+    public static void main(String[] args) {
+    //    halfPyramid(5);
+       inverted_Half_Pyramid(5);
     }
 }
