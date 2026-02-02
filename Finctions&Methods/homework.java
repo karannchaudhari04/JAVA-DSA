360e1167e62c02015e3791ff59d11a3d668f1874
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-02 12:12:36 +0530

    Palindrome using function

--- Finctions&Methods/homework.java
+++ Finctions&Methods/homework.java
@@ -1,11 +1,32 @@
 public class homework {
-    public static int avg(int a, int b, int c) {
-        int sum = a + b + c;
-        int average = sum / 3;
-        System.out.println("Average is: " + average);
-        return average;
+    public static void average(int a, int b, int c) {
+        int avg = (a + b + c) / 3;
+        System.out.println("The average is: " + avg);
+    }
+    public static boolean isEven(int n){
+        if(n % 2 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void isPalindrome(int n){
+        int original = n;
+        int rev = 0;
+        while(n > 0){
+            int digit = n % 10;
+            rev = rev * 10 + digit;      
+            n /= 10;
+        }
+        if(original == rev){
+            System.out.println(original+" is a palindrome.");
+        } else {
+            System.out.println(original+" is not a palindrome.");
+        }
     }
     public static void main(String[] args) {
-        avg(10, 20, 30);
+        average(10, 20, 30);
+        System.out.println(isEven(1110));
+        isPalindrome(121);
     }
 }
