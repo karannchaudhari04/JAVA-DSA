be7ef6487266ee947e67407191cf3abcdda7f03e
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-04 12:35:14 +0530

    .

--- Patterns/pattern3.java
+++ Patterns/pattern3.java
@@ -1,5 +1,20 @@
 package Patterns;
 
 public class pattern3 {
-    
+    public static void binary_Triangle(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if ((i+j)%2==0) {
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        binary_Triangle(5);
+    }
 }
