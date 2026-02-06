3a2bd90ac885806acd2d8a86170e939b645689fa
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-06 12:10:20 +0530

    String Linear Search

---
+++ Arrays/example.java
@@ -0,0 +1,21 @@
+public class example {
+    public static int find(String fruits[], String key){
+        for(int i=0; i<fruits.length; i++){
+            if(fruits[i].equals(key)){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String fruits[] = {"Apple", "Banana", "Mango", "Orange"};
+        String key = "Orange";
+        int index = find(fruits, key);
+        if(index == -1){
+            System.out.println("Not Found");
+        }
+        else{
+            System.out.println("Key is at index: "+index);
+        }
+    }
+}
