6e65d130f691d89223d19ce5d9f651dee48e74cb
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-06 23:59:38 +0530

    Print Total SubArrays in an Array

--- Arrays/SubArrays.java
+++ Arrays/SubArrays.java
@@ -1,14 +1,17 @@
 public class SubArrays{
     public static void sub_Arrays(int numbers[]) {
+        int ts = 0;
         for(int i=0; i<numbers.length; i++){
             for(int j=i; j<numbers.length; j++){
                 for(int k=i; k<=j; k++){
                     System.out.print(numbers[k]+" ");
                 }
                 System.out.println();
+                ts++;
             }
             System.out.println();
         }
+        System.out.println("Total SubArrays = "+ts);
     }
     public static void main(String[] args) {
         int numbers[] = {2, 4, 6, 8, 10};
