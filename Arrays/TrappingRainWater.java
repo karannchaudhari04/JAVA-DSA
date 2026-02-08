3a6fbd45cd5fbced9325194a35c9a62c555e3625
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-08 12:05:52 +0530

    Trapped Rainwater Problem Solved

--- Arrays/TrappingRainWater.java
+++ Arrays/TrappingRainWater.java
@@ -30,6 +30,5 @@ public class TrappingRainWater {
     public static void main(String[] args) {
         int height[] = {4, 2, 0, 6, 3, 2, 5};
         System.out.println(trapped_Water(height));
-        
     }
 }
