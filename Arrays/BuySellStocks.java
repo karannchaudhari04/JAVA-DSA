f19be7834196a5993024aab6c284b553484df19c
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-12 00:00:09 +0530

    Buy and sell stocks

---
+++ Arrays/BuySellStocks.java
@@ -0,0 +1,24 @@
+public class BuySellStocks {
+    public static int buy_sell_stocks(int prices[]){
+        int n = prices.length;
+        //
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<n; i++){
+            if(buyPrice < prices[i]){ //profit
+                int profit = prices[i] - buyPrice; //Today's Profit
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+        
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(buy_sell_stocks(prices));
+    }
+}
