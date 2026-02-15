9906a7607668268d050527011ab3007b3e596a72
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-15 12:09:06 +0530

    return true

--- Arrays/HasDuplicates.java
+++ Arrays/HasDuplicates.java
@@ -10,7 +10,7 @@ public class HasDuplicates{
         return false;
     }
     public static void main(String[] args) {
-        int nums[] = {1,2,3,1};
+        int nums[] = {1,2,3,4,5,6,7,8,9,10,4};
         System.out.println(is_Distinct(nums));
     }
 }
