b06585a71e03e6b6b040d5aca00c8dc9fcf9e9d6
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-17 23:32:47 +0530

    Solved Second Largest Number Question

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -4,7 +4,7 @@ public class SecondLargest{
         int secondLargest = Integer.MIN_VALUE;
 
         for(int i=0; i<nums.length; i++){
-            if(nums[i]>largest){
+            if(nums[i] > largest){
                 secondLargest = largest;
                 largest = nums[i];
             }
