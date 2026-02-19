47d86579d74277effde8eb43e1878268bf433f48
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-19 18:41:28 +0530

    Solved Spiral Matrix Question For 2D Arrays

---
+++ 2DArrays/main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class main {
+
+    public static void search(int matrix[][], int key){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == key){
+                   System.out.print("Found at cell ("+i+","+j+")");
+                } 
+            }
+        }
+        System.out.println("Not found");
+    }
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+
+        Scanner sc = new Scanner(System.in);
+        //input
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        //output
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        search(matrix, 8);
+    }
+}
