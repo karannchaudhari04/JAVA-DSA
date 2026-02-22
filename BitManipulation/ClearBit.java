38dbdb6ef715bf4be35cd84e26dea1cbc811cdda
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-22 18:45:10 +0530

    Set ith Bit

---
+++ BitManipulation/ClearBit.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+public class ClearBit {
+    public static int clearBit(int n, int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(clearBit(10, 2));
+    }
+}
---
+++ BitManipulation/SetBit.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class SetBit {
+    public static int setBit(int n, int i) {
+        int bitMask = 1<<i;
+        return n | bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(setBit(10, 2));
+    }
+}
