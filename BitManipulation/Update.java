e129b78aedd9f4e6fde67e52e0dafd25209bd36e
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-02-24 20:47:13 +0530

    Update ith Bit

---
+++ BitManipulation/Update.java
@@ -0,0 +1,22 @@
+package BitManipulation;
+public class Update {
+    public static int setBit(int n, int i) {
+        int bitMask = 1<<i;
+        return n | bitMask;
+    }
+
+    public static int clearBit(int n, int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+
+    }
+
+    public static int update(int n, int i, int newBit){
+        n = clearBit(n, i);
+        int bitMask = newBit << i;
+        return n | bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(update(10, 2, 1));
+    }
+}
