d1e19075d64a8420e5e9713ac574a4d7568171ca
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-03-02 20:47:43 +0530

    Methoad Overriding in java

--- OOP/MethodOverriding.java
+++ OOP/MethodOverriding.java
@@ -1,5 +1,20 @@
 package OOP;
 
 public class MethodOverriding {
-    
+    public static void main(String[] args) {
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("eats anything...");
+    }
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("eats grass...");
+    }
 }
