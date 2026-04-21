7d315e17b7a97ab02b6c4fc753cacce9184a4f5a
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-04-21 12:08:28 +0530

    Updated half Pyramid code with numbers instead of stars

--- Patterns/pattern.java
+++ Patterns/pattern.java
@@ -4,12 +4,11 @@ public class pattern {
 
     public static void halfPyramid(int n){
         for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            System.out.print("*");
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
         }
-        System.out.println();
-       }
-       System.out.println();
     }
     public static void main(String[] args) {
         halfPyramid(5);
