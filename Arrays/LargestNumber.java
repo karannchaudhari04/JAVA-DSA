490293188d54c97ba3e70b934d57b7637df9272c
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-04-25 20:45:55 +0530

    Refactor largest_Number method to simplify logic and remove unused smallest value calculation

--- Arrays/LargestNumber.java
+++ Arrays/LargestNumber.java
@@ -1,22 +1,17 @@
 public class LargestNumber {
-    public static int largest_Number(int numbers[]){
-        int largest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
-        for(int i=0; i<numbers.length; i++){
-            if(largest < numbers[i]){
-                largest = numbers[i];
-            }
-            if(smallest > numbers[i]){
-                smallest = numbers[i];
+    public static int largest_Number(int nums[]){
+        int largest = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(largest < nums[i]){
+                largest = nums[i];
             }
         }
-        System.out.println("Smallest value in an Array is: "+smallest);
         return largest;
         
     }
     public static void main(String[] args) {
-        int numbers[] = {1, 2, 6, 3, 5};
-        int largest = largest_Number(numbers);
+        int nums[] = {1, 2, 6, 3, 5};
+        int largest = largest_Number(nums);
         System.out.println("Largest value in an Array is: "+largest);
     }
 }
