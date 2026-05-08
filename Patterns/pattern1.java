5c8cac5a08476e869cefbc3225c4c3361969ca15
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-05-08 11:56:01 +0530

    Refactor Pattern1 class to allow multiple pattern sizes input

--- Patterns/pattern1.java
+++ Patterns/pattern1.java
@@ -13,7 +13,10 @@ public class Pattern1 {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        firstPattern(n);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            int n = sc.nextInt();
+            firstPattern(n);
+        }
     }
 }
