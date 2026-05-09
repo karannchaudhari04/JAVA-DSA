230f1a2e1ac4b8eee789940ab8c65c604b0a10cf
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-05-09 23:47:35 +0530

    Refactor Pattern9 class to improve code readability by adding comments for space and star loops

--- Patterns/Pattern9.java
+++ Patterns/Pattern9.java
@@ -3,25 +3,31 @@ package Patterns;
 public class Pattern9 {
     public static void ninethPattern1(int n){
         for(int i=1; i<=n; i++){
-            for(int  j=1; j<=n-i; j++){
+            //space
+            for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            for(int  j=1; j<=2*i-1; j++){
+            //stars
+            for(int j=1; j<=2*i-1; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        
     }
     public static void ninethPattern2(int n){
         for(int i=1; i<=n; i++){
-            for(int  j=1; j<=i-1; j++){
+            //space
+            for(int j=1; j<=i-1; j++){
                 System.out.print(" ");
             }
-            for(int  j=1; j<=2*n-(2*i-1); j++){
+            //stars
+            for(int j=1; j<=2 * n - (2 * i - 1); j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        
     }
     public static void main(String[] args) {
         ninethPattern1(5);
