536a284865a36082e6f4362812c46edd4d7c9907
Karan Chaudhari <149130038+karannchaudhari04@users.noreply.github.com>
2026-06-26 15:03:28 +0530

    CountDigitInNumber.java

--- Basics/CountDigitInNumber.java
+++ Basics/CountDigitInNumber.java
@@ -16,7 +16,6 @@ public class CountDigitInNumber{
         System.out.println("Total count = "+count);
         return count;
     }
-    
     public static void main(String[] args) {
         count(456450);
         count1(9465);
