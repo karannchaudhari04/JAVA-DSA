3fdd0246cd8d04d08ed0730aed27f2cbb0bdf479
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-06-27 14:54:04 +0530

    implement two-sum problem solution

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 3, 7, 4, 3, 8};
+        int target = 9;
+
+        TwoSum ts = new TwoSum();
+
+        int[] result = ts.twoSum(nums, target);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
