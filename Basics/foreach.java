b057de443c090f95df174954639b525fbd5fd3db
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-01 23:52:11 +0530

    foreach.java to demonstrate enhanced for-loop usage

---
+++ Basics/foreach.java
@@ -0,0 +1,9 @@
+public class foreach{
+    public static void main(String args[]){
+        int arr[] = {10,20,30,40,50};
+
+        for(int val : arr){
+            System.out.println(val);
+        }
+    }
+}
