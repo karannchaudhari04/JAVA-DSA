d45e33ab3e000c2afedffd3265b06299e49740fe
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-03 22:00:13 +0530

    Add SumOf2DArray class to calculate the sum of elements in a 2D array

---
+++ 2DArrays/SumOf2DArray.java
@@ -0,0 +1,18 @@
+public class SumOf2DArray {
+    public static void main(String[] args) {
+        int arr[][] = {
+                        {10,20},
+                        {30},
+                        {40,50,60,70},
+                        {80,90,100}
+                    };
+        
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                sum = sum + arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
