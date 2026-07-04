a3da225b0c6eca946c024a20b86d1807646f5b61
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-04 23:54:19 +0530

    Add index class to find the maximum element in a jagged array

---
+++ Strings/index.java
@@ -0,0 +1,20 @@
+public class index {
+    public static void main(String[] args) {
+        int arr[][] = {
+                        {10,20},
+                        {30},
+                        {40,500,60,70},
+                        {80,90,100}
+                    };
+        int max = arr[0][0];
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                if(arr[i][j]>max){
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
