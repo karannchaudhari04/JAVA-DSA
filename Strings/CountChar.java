d654952e335f7ba6807c8c098cfa80d23ee7a243
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-06 00:22:37 +0530

    Add CountChar and ReverseString classes for character counting and string reversal

---
+++ Strings/CountChar.java
@@ -0,0 +1,9 @@
+public class CountChar{
+    public static void main(String[] args) {
+        String str = "Karan Chaudhari";
+        
+        char[] arr = str.toCharArray();
+        int len = arr.length;
+        System.out.println(len);
+    }
+}
---
+++ Strings/ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Karan Chaudhari";
+
+        String reverse = "";
+        int n = str.length();
+        for(int i=n-1; i>=0; i--){
+            char ch = str.charAt(i);
+            reverse = reverse + ch;
+        }
+        System.out.println(reverse);
+    }
+}
