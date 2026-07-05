b4988d073b5a977d6694501d7a8e1e8bd0451dfe
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-05 17:31:43 +0530

    string methods

--- Strings/Basics.java
+++ Strings/Basics.java
@@ -2,5 +2,7 @@ public class Basics {
     public static void main(String[] args) {
         String name = new String("Karan Chaudhari");
         System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.charAt(4));
     }
 }
