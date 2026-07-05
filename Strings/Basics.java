8e6b3c8fe6088743210f60155300177428ba90dc
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-05 20:01:06 +0530

    Add string comparison logic to Basics class

--- Strings/Basics.java
+++ Strings/Basics.java
@@ -4,5 +4,15 @@ public class Basics {
         System.out.println(name);
         System.out.println(name.length());
         System.out.println(name.charAt(4));
+
+        String name1 = "Karan";
+        String name2 = "Karan";
+
+        if(name1 == name2){
+            System.out.println("Both are equals");
+        }
+        else{
+            System.out.println("Both are not equals");
+        }
     }
 }
