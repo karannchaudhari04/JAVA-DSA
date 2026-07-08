ef17f3b92d6021ec89b42b28294629119f63c5d3
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-08 20:08:42 +0530

    Add digit counting functionality to PrintDigits class

--- Basics/PrintDigits.java
+++ Basics/PrintDigits.java
@@ -3,11 +3,13 @@ package Basics;
 public class PrintDigits {
     public static void main(String[] args) {
         int number = 546849;
-
+        int count = 0;
         while(number != 0){
             int lastDigit = number % 10;
             System.out.println(lastDigit);
+            count++;
             number = number / 10;
         }
+        System.out.println("Total digits in a number are: "+count);
     }
 }
