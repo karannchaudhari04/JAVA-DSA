ea9cd7095a9a45b49107925b21d68502a99d9412
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-08 20:10:09 +0530

    Add sum calculation for digits in PrintDigits class

--- Basics/PrintDigits.java
+++ Basics/PrintDigits.java
@@ -4,12 +4,15 @@ public class PrintDigits {
     public static void main(String[] args) {
         int number = 546849;
         int count = 0;
+        int sum = 0;
         while(number != 0){
             int lastDigit = number % 10;
             System.out.println(lastDigit);
+            sum = sum + lastDigit;
             count++;
             number = number / 10;
         }
         System.out.println("Total digits in a number are: "+count);
+        System.out.println("Sum of all digits in a number is: "+sum);
     }
 }
