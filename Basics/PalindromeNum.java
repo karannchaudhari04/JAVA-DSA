d8270a97cfaf39a80821f57e3ed31b3e32a35bdc
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-09 12:19:42 +0530

    Refactor reverse number logic and add PalindromeNum class for palindrome checking

---
+++ Basics/PalindromeNum.java
@@ -0,0 +1,25 @@
+package Basics;
+
+public class PalindromeNum {
+    public static boolean palindrome(int num){
+        int original = num;
+        int reverse = 0;
+        while(num != 0){
+            int lastDigit = num % 10;
+            reverse = reverse * 10 + lastDigit;
+            num = num / 10;
+        }
+        if(original == reverse){
+            System.out.println(original+" is a palindrome number");
+            return true;
+        }
+        else{
+            System.out.println(original+" is not a palindrome number");
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int num = 54345;
+        palindrome(num);
+    }
+}
--- Basics/reverseanumber.java
+++ Basics/reverseanumber.java
@@ -2,13 +2,13 @@ package Basics;
 
 public class reverseanumber {
     public static void main(String[] args) {
-        int n = 110899;
-        int rev = 0;
-        while(n>0){
-            int lastdigit = n%10;
-            rev = rev * 10 + lastdigit;
-            n = n/10;
+        int num = 236469;
+        int revNumber = 0;
+        while(num != 0){
+            int lastDigit = num % 10;
+            revNumber = revNumber * 10 + lastDigit;
+            num = num / 10;
         }
-        System.out.println(rev);
+        System.out.println(revNumber);
     }
 }
