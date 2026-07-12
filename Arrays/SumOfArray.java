7ce5cf81c886ea8de5ebd7e325bc2ea899525632
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-12 19:43:52 +0530

    Fix sum calculation in SumOfArray class to correctly sum array elements

--- Arrays/SumOfArray.java
+++ Arrays/SumOfArray.java
@@ -1,13 +1,11 @@
 public class SumOfArray{
-    public static void main(String args[]){
+    public static void main(String[] args) {
         int arr[] = {10,20,30,40,50};
-        int n = arr.length;
         int sum = 0;
 
-        for(int i=0; i<n; i++){
-            sum = sum+arr[i];
+        for(int i=0; i<arr.length; i++){
+            sum = sum + i;
         }
-
         System.out.println(sum);
     }
 }
