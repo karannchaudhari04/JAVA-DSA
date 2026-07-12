3f6308c50933431dbe3fe2db8ee08acbe7819c9f
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-12 19:52:06 +0530

    Refactor Creation class to update array initialization and improve output formatting

--- 2DArrays/Creation.java
+++ 2DArrays/Creation.java
@@ -1,20 +1,16 @@
 public class Creation {
     public static void main(String[] args) {
         int[][] arr = {
-                    {1,2},
-                    {2,3,5,1,3},
-                    {3,4,64,1},
-                    {4}
-                };
-                System.out.println(arr[2][2]);
-
-                int rowLength = arr.length;
-
-                for(int i=0; i<rowLength; i++){
-                    for(int j=0; j<arr[i].length; j++){
-                        System.out.print(arr[i][j] + " ");
-                    }
-                    System.out.println();
-                }
+                        {10,20},
+                        {30,40,50},
+                        {60},
+                        {70,80,90,100}
+        };
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
