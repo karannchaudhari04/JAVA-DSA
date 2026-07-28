8dbb400fd55b572fd21a7b3fd0596c45fe8e1f16
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-07-28 12:26:39 +0530

    Refactor Print1toN class for consistency in variable naming and add backtrack2 class for backtracking demonstration

--- Recursion/backtrack2.java
+++ Recursion/backtrack2.java
@@ -1,15 +1,15 @@
-public class Print1toN{
+public class backtrack2 {
     public static void print(int n){
         helper(1,n);
     }
+    
     private static void helper(int i, int n){
         if(i > n){
             return;
         }
-        System.out.println(i);
         helper(i+1, n);
+        System.out.println(i);
     }
-    
     public static void main(String[] args) {
         int n = 5;
         print(n);
