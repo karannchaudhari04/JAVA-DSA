da319baa00398375932f3fac4d1e715c8346466d
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-09-25 12:29:14 +0530

    Refactor average method to return int and simplify calculation

--- Arrays/ArrayAverage.java
+++ Arrays/ArrayAverage.java
@@ -1,15 +1,15 @@
 public class ArrayAverage{
-    public static double average(int arr[]){
-        double sum = 0;
-        for(int i=0; i<arr.length; i++){
+    public static int average(int arr[]){
+        int sum = 0;
+        int n = arr.length;
+        for(int i=0; i<n; i++){
             sum = sum + arr[i];
         }
-        int size = arr.length;
-        double average = sum / size;
+        int average = sum/n;
         return average;
     }
-    public static void main(String args[]){
-        int arr[] = {10,20,30,40,50};
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
         System.out.println(average(arr));
     }
 }
